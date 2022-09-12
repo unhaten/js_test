6ac474de99986baa4c06a4c6a8efefd395c2382a
unhaten <csgold2002@mail.ru>
2022-09-13 00:57:37 +0300

    next part

---
+++ project/static/js/index.js
@@ -0,0 +1,27 @@
+// document.getElementById("counter").innerText = 12;
+
+// let countEl = document.getElementById('count-el')
+
+// let count = 0;
+
+// function increment() {
+//     count = count + 1;
+//     countEl.innerText = count
+// }
+
+// function save() {
+//     console.log(countEl.innerText)
+// }
+
+//! ----------------------------------------
+
+// let name = "Max";
+
+// let greeting = "Hi, my name is ";
+
+// let myGreeting = greeting + name;
+
+// console.log(myGreeting);
+
+//! ----------------------------------------
+
