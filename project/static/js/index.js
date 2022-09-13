a8dab5a00627dd42b4a5decddb09e7bfffd6abb1
unhaten <csgold2002@mail.ru>
2022-09-13 21:18:14 +0300

    ending with counter / starting blackjack

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -1,16 +1,21 @@
 // document.getElementById("counter").innerText = 12;
 
-// let countEl = document.getElementById('count-el')
+//? TASK WITH COUNTING NEW 'PASSENGERS/ENTRIES'
+// let countEl = document.getElementById("count-el");
+// let saveEl = document.getElementById("save-el");
 
 // let count = 0;
 
 // function increment() {
-//     count = count + 1;
-//     countEl.innerText = count
+//     count += 1;
+//     countEl.textContent = count;
 // }
 
 // function save() {
-//     console.log(countEl.innerText)
+//     saveEl.textContent += countEl.textContent + " - ";
+//     countEl.textContent = 0;
+//     count = 0;
+// console.log(countEl.innerText);
 // }
 
 //! ----------------------------------------
@@ -24,4 +29,62 @@
 // console.log(myGreeting);
 
 //! ----------------------------------------
+
+// let welcomeEl = document.getElementById("welcome-el");
+
+// name = "Max";
+// greeting = "Morning, ";
+
+// welcomeEl.innerText = greeting + name;
+
+// welcomeEl.innerText += "😂😂😂";
+
+//! ----------------------------------------
+
+// let myPoints = 3;
+
+// function add3Points() {
+//     myPoints += 3;
+// }
+
+// function remove1Point() {
+//     myPoints -= 1;
+// }
+
+// add3Points();
+// add3Points();
+// add3Points();
+// remove1Point();
+// remove1Point();
+// console.log(myPoints);
+
+//! ------------------------------
+
+// let num1 = 8;
+// let num2 = 2;
+// document.getElementById("num1-el").textContent = num1;
+// document.getElementById("num2-el").textContent = num2;
+// let sumEl = document.getElementById("sum-el");
+
+// function add() {
+//     let sum = num1 + num2;
+//     sumEl.textContent = sum;
+// }
+
+// function subtract() {
+//     let sum = num1 - num2;
+//     sumEl.textContent = sum;
+// }
+
+// function divide() {
+//     let sum = num1 / num2;
+//     sumEl.textContent = sum;
+// }
+
+// function multiply() {
+//     let sum = num1 * num2;
+//     sumEl.textContent = sum;
+// }
+
+//! ----------------------------------------------------------------
 
